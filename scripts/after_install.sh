088d5315bd57af41b58343834d54ff4f4198289f
maheshfinpros <166098099+maheshfinpros@users.noreply.github.com>
2024-05-27 12:38:57 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,10 +5,10 @@ INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
 if [[ $INSTANCE_ID == "i-09759cd2f95e9f5f9" ]]; then
   echo "Instance 1 detected. Moving index1.html to index.html"
-  mv /var/www/html/index1.html /var/www/html/index.html
+  cp /var/www/html/index1.html /var/www/html/index.html
 elif [[ $INSTANCE_ID == "i-06b7c8a20f24b5af5" ]]; then
   echo "Instance 2 detected. Moving index2.html to index.html"
-  mv /var/www/html/index2.html /var/www/html/index.html
+  cp /var/www/html/index2.html /var/www/html/index.html
 else
   echo "Instance ID does not match known instances."
 fi
