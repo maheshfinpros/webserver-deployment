adc8ca2fb9c307f9a122fae755e6d718025f4b31
maheshfinpros <166098099+maheshfinpros@users.noreply.github.com>
2024-05-27 13:06:46 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,10 +8,12 @@ echo "Running after_install.sh" > $LOG_FILE
 
 if [[ $INSTANCE_ID == "i-09759cd2f95e9f5f9" ]]; then
   echo "Instance 1 detected. Moving index1.html to index.html" >> $LOG_FILE
-  cp /var/www/html/index1.html /var/www/html/index.html
+rm -rf /var/www/html/index.html
+  mv /var/www/html/index1.html /var/www/html/index.html
 elif [[ $INSTANCE_ID == "i-06b7c8a20f24b5af5" ]]; then
   echo "Instance 2 detected. Moving index2.html to index.html" >> $LOG_FILE
-  cp /var/www/html/index2.html /var/www/html/index.html
+  rm -rf /var/www/html/index.html
+  mv /var/www/html/index2.html /var/www/html/index.html
 else
   echo "Instance ID does not match known instances." >> $LOG_FILE
 fi
